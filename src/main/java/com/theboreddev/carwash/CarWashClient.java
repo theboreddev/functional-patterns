206c0a111edf783a202146ab5dc081a579498812
theboreddev <account@theboreddev.com>
2020-07-05 10:38:24 +0100

    Add functional chain of responsibility pattern

--- src/main/java/com/theboreddev/carwash/CarWashClient.java
+++ src/main/java/com/theboreddev/carwash/CarWashClient.java
@@ -1,19 +1,22 @@
 package com.theboreddev.carwash;
 
+import java.util.function.Function;
+
 public class CarWashClient {
 
     public static void main(String[] args) {
 
         final Car car = new Car();
+        Function<Car, Car> initial = c -> new Car(WashState.INITIAL);
 
-        final CarWashStep chain = new InitialWashStep();
+        final Function<Car, Car> chain = initial
+                .andThen(c1 -> new Car(WashState.INITIAL_WASH))
+                .andThen(c2 -> new Car(WashState.SOAP))
+                .andThen(c3 -> new Car(WashState.RINSED))
+                .andThen(c4 -> new Car(WashState.POLISHED))
+                .andThen(c5 -> new Car(WashState.DRIED));
 
-        chain.andThen(new SoapStep())
-                .andThen(new RinseStep())
-                .andThen(new PolishStep())
-                .andThen(new DryStep());
-
-        final Car finalCar = chain.applyTo(car);
+        final Car finalCar = chain.apply(car);
 
         System.out.println("Final car state is " + finalCar.washState());
     }
---
+++ src/main/java/com/theboreddev/carwash/OldStyleCarWashClient.java
@@ -0,0 +1,20 @@
+package com.theboreddev.carwash;
+
+public class OldStyleCarWashClient {
+
+    public static void main(String[] args) {
+
+        final Car car = new Car();
+
+        final CarWashStep chain = new InitialWashStep();
+
+        chain.andThen(new SoapStep())
+                .andThen(new RinseStep())
+                .andThen(new PolishStep())
+                .andThen(new DryStep());
+
+        final Car finalCar = chain.applyTo(car);
+
+        System.out.println("Final car state is " + finalCar.washState());
+    }
+}
